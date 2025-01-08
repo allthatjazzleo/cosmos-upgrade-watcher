fda4c73cec3679280c9cbc7c71fdf6d19af83dbf
allthatjazzleo <leo.pang@mantra.finance>
2025-01-08 19:56:17 +0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 mod config;
 
+use chrono::{DateTime, Utc};
 use clap::Parser;
 use config::Config;
+use prometheus::{register_int_gauge_vec, Encoder, IntGaugeVec, TextEncoder};
 use reqwest::Client;
 use serde::Deserialize;
-use tokio::time::interval;
-use prometheus::{Encoder, TextEncoder, register_int_gauge_vec, IntGaugeVec};
-use warp::Filter;
-use std::net::IpAddr;
-use chrono::{Utc, DateTime};
 use std::collections::HashSet;
-use tokio::sync::Mutex;
+use std::net::IpAddr;
 use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio::time::interval;
+use warp::Filter;
 
 #[derive(Deserialize, Clone, Eq, Hash, PartialEq, Debug)]
 struct ChainUpgrade {
@@ -33,14 +33,18 @@ async fn main() {
     let opts: Opts = Opts::parse();
     let config = Config::from_file(&opts.config);
 
-    println!("Watching the following chains: {:?}", config.chain.watch_list);
+    println!(
+        "Watching the following chains: {:?}",
+        config.chain.watch_list
+    );
 
     let client = Client::new();
     let gauge = register_int_gauge_vec!(
         "chain_upgrade",
         "Chain upgrade information",
         &["network", "node_version", "block"]
-    ).unwrap();
+    )
+    .unwrap();
 
     let watch_list: Vec<String> = config.chain.watch_list.clone();
     let refresh_interval = humantime::parse_duration(&config.chain.refresh).unwrap();
@@ -57,26 +61,36 @@ async fn main() {
         let mut interval = interval(refresh_interval);
         loop {
             interval.tick().await;
-            match fetch_and_update_metrics(&client_clone, &endpoint_clone, &watch_list_clone, &gauge_clone, &upgrades_clone).await {
+            match fetch_and_update_metrics(
+                &client_clone,
+                &endpoint_clone,
+                &watch_list_clone,
+                &gauge_clone,
+                &upgrades_clone,
+            )
+            .await
+            {
                 Ok(_) => println!("Metrics updated successfully"),
                 Err(e) => eprintln!("Failed to update metrics: {}", e),
             }
         }
     });
 
-    let metrics_route = warp::path("metrics")
-        .map(move || {
-            let encoder = TextEncoder::new();
-            let metric_families = prometheus::gather();
-            let mut buffer = Vec::new();
-            encoder.encode(&metric_families, &mut buffer).unwrap();
-            warp::http::Response::builder()
-                .header("Content-Type", encoder.format_type())
-                .body(buffer)
-        });
+    let metrics_route = warp::path("metrics").map(move || {
+        let encoder = TextEncoder::new();
+        let metric_families = prometheus::gather();
+        let mut buffer = Vec::new();
+        encoder.encode(&metric_families, &mut buffer).unwrap();
+        warp::http::Response::builder()
+            .header("Content-Type", encoder.format_type())
+            .body(buffer)
+    });
 
     warp::serve(metrics_route)
-        .run((config.prometheus.host.parse::<IpAddr>().unwrap(), config.prometheus.port))
+        .run((
+            config.prometheus.host.parse::<IpAddr>().unwrap(),
+            config.prometheus.port,
+        ))
         .await;
 }
 
@@ -87,20 +101,28 @@ async fn fetch_and_update_metrics(
     gauge: &IntGaugeVec,
     upgrades: &Arc<Mutex<HashSet<ChainUpgrade>>>,
 ) -> Result<(), reqwest::Error> {
-    let response = client.get(endpoint).send().await?.json::<Vec<ChainUpgrade>>().await?;
+    let response = client
+        .get(endpoint)
+        .send()
+        .await?
+        .json::<Vec<ChainUpgrade>>()
+        .await?;
     let now = Utc::now();
     let mut upgrades_guard = upgrades.lock().await;
 
     // Add or update metrics
     for upgrade in &response {
         if watch_list.contains(&upgrade.network) {
-            let upgrade_time = DateTime::parse_from_rfc3339(&upgrade.estimated_upgrade_time).unwrap();
+            let upgrade_time =
+                DateTime::parse_from_rfc3339(&upgrade.estimated_upgrade_time).unwrap();
             if now < upgrade_time {
-                gauge.with_label_values(&[
-                    &upgrade.network,
-                    &upgrade.node_version,
-                    &upgrade.block.to_string(),
-                ]).set(1);
+                gauge
+                    .with_label_values(&[
+                        &upgrade.network,
+                        &upgrade.node_version,
+                        &upgrade.block.to_string(),
+                    ])
+                    .set(1);
                 upgrades_guard.insert(upgrade.clone());
             }
         }
@@ -136,7 +158,8 @@ mod tests {
             "test_chain_upgrade",
             "Test chain upgrade information",
             &["network", "node_version", "block"]
-        ).unwrap();
+        )
+        .unwrap();
 
         let upgrades: Arc<Mutex<HashSet<ChainUpgrade>>> = Arc::new(Mutex::new(HashSet::new()));
         let watch_list = vec!["testnet".to_string()];
@@ -183,7 +206,9 @@ mod tests {
             &watch_list,
             &gauge,
             &upgrades,
-        ).await.unwrap();
+        )
+        .await
+        .unwrap();
 
         let metric_families = prometheus::gather();
         let mut buffer = Vec::new();
